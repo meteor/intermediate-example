a121f6cd183b5ba5eda58e8071acad993edaa6df
Shavonnah <Sh@vonnah.com>
2016-02-28 23:38:59 -0800

    Step 7.5: Hide form fields

--- collections/recipes.js
+++ collections/recipes.js
@@ -14,6 +14,9 @@ RecipeSchema = new SimpleSchema({
 		label: "Author",
 		autoValue: function () {
 			return this.userId
+		},
+		autoform: {
+			type: "hidden"
 		}
 	},
 	createdAt: {
@@ -21,6 +24,9 @@ RecipeSchema = new SimpleSchema({
 		label: "Created At",
 		autoValue: function() {
 			return new Date()
+		},
+		autoform: {
+			type: "hidden"
 		}
 	}
 });
