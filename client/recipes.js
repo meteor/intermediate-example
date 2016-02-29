26c40cd873d707c1b7141f11cd403bbfeb879102
Shavonnah <Sh@vonnah.com>
2016-02-29 04:11:15 -0800

    Step 13.1: Create template level subscription

--- client/recipes.js
+++ client/recipes.js
@@ -1,4 +1,9 @@
-Meteor.subscribe('recipes');
+Template.Recipes.onCreated(function(){
+	var self = this;
+	self.autorun(function() {
+		self.subscribe('recipes');
+	});
+});
 
 Template.Recipes.helpers({
 	recipes: ()=> {
