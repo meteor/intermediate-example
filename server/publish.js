123c0a8bcf71718c5a444824bc8cc043b5dbc708
Shavonnah <Sh@vonnah.com>
2016-03-01 10:21:10 -0800

    Step 15.1: Create a publication for a single recipe

--- server/publish.js
+++ server/publish.js
@@ -1,3 +1,8 @@
 Meteor.publish('recipes', function(){
 	return Recipes.find({author: this.userId});
 });
+
+Meteor.publish('SingleRecipe', function(id){
+	check(id, String);
+	return Recipes.find({_id: id});
+});
