47752f51e0e952ae99495e8a2eb1f156e2871989
Shavonnah <Sh@vonnah.com>
2016-03-01 16:55:49 -0800

    Step 22.2: Add a helper for updateRecipeId

--- client/recipes/recipe.js
+++ client/recipes/recipe.js
@@ -1,3 +1,9 @@
+Template.Recipe.helpers({
+	updateRecipeId: function() {
+		return this._id;
+	}
+});
+
 Template.Recipe.events({
 	'click .toggle-menu': function(){
 		Meteor.call('toggleMenuItem', this._id, this.inMenu);
