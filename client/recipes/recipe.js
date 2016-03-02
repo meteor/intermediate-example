04de40a9702c04ea6f8071602470ddc748a00e3f
Shavonnah <Sh@vonnah.com>
2016-03-01 21:43:03 -0800

    Step 23.3: Call the delete method when a user clicks the trash icon

--- client/recipes/recipe.js
+++ client/recipes/recipe.js
@@ -7,5 +7,8 @@ Template.Recipe.helpers({
 Template.Recipe.events({
 	'click .toggle-menu': function(){
 		Meteor.call('toggleMenuItem', this._id, this.inMenu);
+	},
+	'click .fa-trash': function () {
+		Meteor.call('deleteRecipe', this._id);
 	}
 });
